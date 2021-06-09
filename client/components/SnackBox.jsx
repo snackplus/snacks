f6251272da42d3f43d15323e5f3439480eef2989
Nikhil Massand <nikhil.massand@gmail.com>
2021-06-09 10:14:42 -0400

    rendering snack container

--- client/components/SnackBox.jsx
+++ client/components/SnackBox.jsx
@@ -7,17 +7,26 @@ import Box from '@material-ui/core/Box';
 export default function SnackBox (props) {
 
   return (
-    <div key={props.key} id={props.id} className="snackBox">
-        <a href={props.image}></a>
-        <br/>
-        <h3>{props.brand}</h3>
-        {/* image, name, average star rating, number of reviews */}
-        <h3>{props.name}</h3>
-        <h4>{props.origin}</h4>
+      <div key={props.box.snack_id}>
+        <h1>{props.box.snack_id}</h1>
+        <img src={props.box.img} alt="" />
+        {/* <h1>{props.box.img}</h1> */}
+        <h3>{props.box.brand_name} {props.box.snack_name}</h3>
+        <h4>{props.box.origin}</h4>
+        {/* {props.box.map(el => {
+          return (
+            <h1>{el.snack_id}</h1>
+          )
+        })} */}
+      </div>
+    // <div key={props.key} id={props.id} className="snackBox">
+    //     <a href={props.image}></a>
+    //     <br/>
+    //     <h3>{props.brand}</h3>
+    //     {/* image, name, average star rating, number of reviews */}
+    //     <h3>{props.name}</h3>
+    //     <h4>{props.origin}</h4>
       
-    </div>
-      
-
-
+    // </div>
   )
 }
